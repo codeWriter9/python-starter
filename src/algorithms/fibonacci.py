d1228aa23b9c9d5a71b7f9852364a5d531e16d78
codeWriter9 <Sanjay.X.Ghosh@gmail.com>
2020-10-15 14:30:23 +0530

    Add - sample file in different package

---
+++ src/algorithms/fibonacci.py
@@ -0,0 +1,7 @@
+def fibonacci(term):
+    if term == 1:
+        return 1;
+    elif term == 2:
+        return 1;
+    else :
+        return (fibonacci(term - 1) + fibonacci(term - 2));
---
+++ tests/test_fibonacci.py
@@ -0,0 +1,21 @@
+# the inclusion of the tests module is not meant to offer best practices for
+# testing in general, but rather to support the `find_packages` example in
+# setup.py that excludes installing the "tests" package
+
+import unittest
+
+from algorithms.fibonacci import fibonacci
+
+
+class TestSimple(unittest.TestCase):
+
+    def test_fibonacci(self):
+        self.assertEqual(fibonacci(1), 1)
+        self.assertEqual(fibonacci(2), 1)
+        self.assertEqual(fibonacci(3), 2)
+        self.assertEqual(fibonacci(4), 3)
+        self.assertEqual(fibonacci(5), 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
